bf4997b9be8c009fca676829daa12ba22a3f4bd1
Deepak Kumar Jain <djain@falabella.cl>
2019-09-21 01:08:30 +0530

    feature: add made with bulma badge

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,10 +9,13 @@ const Footer = () => {
       <div className="content has-text-white has-text-centered">
         <p>
           © 2019 &amp; ℗ by <a className="has-text-white" href="https://www.deepakkumarjain.com" rel="noopener noreferrer" target="_blank">Deepak Kumar Jain</a>.
-    </p>
+          <a href="https://bulma.io" className="is-pulled-right line-height-0">
+            <img src="https://bulma.io/images/made-with-bulma.png" alt="Made with Bulma" width="128" height="24" />
+          </a>
+        </p>
       </div>
     </footer>
-  );
-}
-
-export default Footer;
+      );
+    }
+    
+    export default Footer;
